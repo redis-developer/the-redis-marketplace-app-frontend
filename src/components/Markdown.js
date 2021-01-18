241f2c841fc34037c6811bda3d005976c2b35ff3
Roland Szoke <fwalkwithm@gmail.com>
2021-01-18 15:59:42 +0100

    Markdown code formatting

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import gfm from 'remark-gfm';
 
 import { useRequest } from '../hooks';
 
+const renderers = {
+  code: ({ language, value }) => {
+    return (
+      <SyntaxHighlighter style={docco} language={language}>
+        {value}
+      </SyntaxHighlighter>
+    );
+  }
+};
+
 export default function Markdown({ url, ...rest }) {
   const { data } = useRequest({ url });
   return (
-    <ReactMarkdown plugins={[gfm]} skipHtml {...rest}>
+    <ReactMarkdown renderers={renderers} plugins={[gfm]} skipHtml {...rest}>
       {data}
     </ReactMarkdown>
   );
