5de3a45dbd6775e212bbb2e08893643c179f8945
Roland Szoke <fwalkwithm@gmail.com>
2021-01-18 16:08:50 +0100

    Fix markdown code formatting

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { docco } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 import gfm from 'remark-gfm';
 
 import { useRequest } from '../hooks';
