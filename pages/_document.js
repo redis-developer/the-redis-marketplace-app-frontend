36ab234c91b5411c637152fc7f62cb6fc424a931
IT Solution <itsolution0820@gmail.com>
2021-07-22 23:08:56 -0400

    Fix UI issues from JIRA

--- pages/_document.js
+++ pages/_document.js
@@ -12,8 +12,11 @@ export default class MyDocument extends Document {
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Mulish&display=swap"
             rel="stylesheet"
           />
         </Head>
