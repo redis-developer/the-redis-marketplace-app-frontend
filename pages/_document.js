e0052e56c4bd06e234afd7922b16a877efbe3a31
IT Solution <itsolution0820@gmail.com>
2021-09-01 15:31:27 -0400

    Build issues

--- pages/_document.js
+++ pages/_document.js
@@ -23,8 +23,7 @@ export default class MyDocument extends Document {
           {/* <!-- Google Tag Manager --> */}
           <script
             dangerouslySetInnerHTML={{
-              _html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
@@ -40,7 +39,10 @@ export default class MyDocument extends Document {
               src="https://www.googletagmanager.com/ns.html?id=GTM-5K5KMFZ"
               height="0"
               width="0"
-              style="display:none;visibility:hidden"></iframe>
+              style={{
+                display: 'none',
+                visibility: 'hidden'
+              }}></iframe>
           </noscript>
           <Main />
           <NextScript />
