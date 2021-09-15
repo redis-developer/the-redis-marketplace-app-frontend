2b0b75f05bd2ac16e1a5925af84f14d2e5c4f330
IT Solution <itsolution0820@gmail.com>
2021-09-15 02:21:56 -0400

    Change google id

--- pages/_document.js
+++ pages/_document.js
@@ -27,7 +27,7 @@ export default class MyDocument extends Document {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5K5KMFZ');`
+          })(window,document,'script','dataLayer','GTM-KPNRRSJ');`
             }}
           />
           {/* <!-- End Google Tag Manager --> */}
@@ -36,7 +36,7 @@ export default class MyDocument extends Document {
           <noscript>
             {/* eslint-disable-next-line jsx-a11y/iframe-has-title */}
             <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-5K5KMFZ"
+              src="https://www.googletagmanager.com/ns.html?id=GTM-KPNRRSJ"
               height="0"
               width="0"
               style={{
